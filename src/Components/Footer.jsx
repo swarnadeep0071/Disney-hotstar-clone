d611c027f8c6f90c94be0b62c5f0f3049f65a6e5
Swarnadeep Dey <swarnadeep2016dey@gmail.com>
2025-02-17 16:45:44 +0530

    Changed in HTML File

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="w-full text-center p-4 bg-gray-800 text-white">
+      &copy; {new Date().getFullYear()} Swarnadeep Dey. AKA.Beast_4k". All
+      rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
